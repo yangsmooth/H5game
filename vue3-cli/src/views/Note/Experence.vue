3b16693ac7244f2ee617a0be217fbee8771625ee
yangsmooth <1126869973@qq.com>
2018-11-29 23:40:49 +0800

    Hbuilder-Vue3cli

---
+++ vue3-cli/src/views/Note/Experence.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="Experence">
+    <h5 class="text-center py-1 mt-0 text-danger">Experence</h5>
+    <ul class="navbar-nav bg-success mx-3 px-0">
+    	<li class="nav-link">1、Do whatever comes to mind(想到啥就做啥)</li>
+    	<li class="nav-link">2</li>
+    	<li class="nav-link">3</li>
+      <li class="nav-link">4</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Experence',
+  props:{
+  	
+  },
+  //ES6д��
+  data(){
+    return {
+				
+	}
+  },
+  computed:{
+		
+  },
+  methods:{
+		
+  },
+  components:{
+  	
+  }
+}
+</script>
+
+
+<style scoped>
+
+li{
+	text-indent: 4px;
+}
+</style>
