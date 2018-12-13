7e6b4ab979b1080d17c363273affc63999987afc
yangsmooth <1126869973@qq.com>
2018-12-13 14:49:26 +0800

    打包

---
+++ vue3-cli/dist1/precache-manifest.af1f3766012eed62886c75807137d02a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90dad8c7983d86eb1df",
+    "url": "assets/css/chunk-79f60387.e9615854.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "e90dad8c7983d86eb1df",
+    "url": "assets/js/chunk-79f60387.9fd37736.js"
+  },
+  {
+    "revision": "4acc0f1be8bcb130f741",
+    "url": "assets/css/app.9f4d20ea.css"
+  },
+  {
+    "revision": "dc65d2d0fdd8b533c1a9",
+    "url": "assets/css/chunk-015757c0.feccb073.css"
+  },
+  {
+    "revision": "dc65d2d0fdd8b533c1a9",
+    "url": "assets/js/chunk-015757c0.ad8d1035.js"
+  },
+  {
+    "revision": "d50838a026c1e887f651",
+    "url": "assets/css/chunk-03d4e898.13288674.css"
+  },
+  {
+    "revision": "d50838a026c1e887f651",
+    "url": "assets/js/chunk-03d4e898.63ea1250.js"
+  },
+  {
+    "revision": "e91baf198e7113181941",
+    "url": "assets/css/chunk-33d55e5e.3ddf9ed8.css"
+  },
+  {
+    "revision": "e91baf198e7113181941",
+    "url": "assets/js/chunk-33d55e5e.adeb1698.js"
+  },
+  {
+    "revision": "bb4deb59baac7282eaab",
+    "url": "assets/css/chunk-3cdc603d.13288674.css"
+  },
+  {
+    "revision": "bb4deb59baac7282eaab",
+    "url": "assets/js/chunk-3cdc603d.ef85a288.js"
+  },
+  {
+    "revision": "203bbbec7254b0d6764b",
+    "url": "assets/css/chunk-47270298.13288674.css"
+  },
+  {
+    "revision": "203bbbec7254b0d6764b",
+    "url": "assets/js/chunk-47270298.d6665af4.js"
+  },
+  {
+    "revision": "e2cadc251737d9e2c7d3",
+    "url": "assets/js/about.bb7115bb.js"
+  },
+  {
+    "revision": "4acc0f1be8bcb130f741",
+    "url": "assets/js/app.80ef2284.js"
+  },
+  {
+    "revision": "711514830af211240aa1",
+    "url": "assets/css/chunk-7cc3b94a.fdc83fcd.css"
+  },
+  {
+    "revision": "711514830af211240aa1",
+    "url": "assets/js/chunk-7cc3b94a.dd6784e1.js"
+  },
+  {
+    "revision": "159cc8086f05a9c67217",
+    "url": "assets/css/chunk-8180b6be.13288674.css"
+  },
+  {
+    "revision": "159cc8086f05a9c67217",
+    "url": "assets/js/chunk-8180b6be.0d362301.js"
+  },
+  {
+    "revision": "f81212f46680fc37f17c",
+    "url": "assets/css/chunk-8dd4183e.21786066.css"
+  },
+  {
+    "revision": "f81212f46680fc37f17c",
+    "url": "assets/js/chunk-8dd4183e.142f87a3.js"
+  },
+  {
+    "revision": "1b398298644b62801443",
+    "url": "assets/js/chunk-vendors.5528631a.js"
+  },
+  {
+    "revision": "b7d4f7ba76de0c544e4ae1b69cb75644",
+    "url": "assets/img/love2.b7d4f7ba.jpg"
+  },
+  {
+    "revision": "dd53f6231570287e2863a962002a8997",
+    "url": "assets/img/love1.dd53f623.jpg"
+  },
+  {
+    "revision": "7052a5794bb6adfff3cdffee32bd58de",
+    "url": "assets/img/love3.7052a579.jpg"
+  },
+  {
+    "revision": "b333ca984e8a4a76de2c668f4932d234",
+    "url": "index.html"
+  },
+  {
+    "revision": "e2cadc251737d9e2c7d3",
+    "url": "assets/css/about.d9b51787.css"
+  }
+];
--- vue3-item/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	baseUrl:'./', //根路径
-	outputDir:'dist2',//构建输出目录
-	assetsDir:'assets',//静态资源目录(js,css,img,fonts)
-	lintOnSave:false//是否开启eslint保存检测，有效值：true ||false||'error'
-};
