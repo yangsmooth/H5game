4f7615abd942431010a28885fac4a203cbd3a4ed
yangsmooth <1126869973@qq.com>
2019-04-03 22:06:58 +0800

    删除所有

--- vue3-cli/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	baseUrl:'./', //根路径
-	outputDir:'dist1',//构建输出目录
-	assetsDir:'assets',//静态资源目录(js,css,img,fonts)
-	lintOnSave:false//是否开启eslint保存检测，有效值：true ||false||'error'
-//	runtimeCompiler: true//是否使用包含运行时编译器的Vue核心的构建。将其设置为true允许您使用templateVue组件中的选项，但会为您的应用带来额外的10kb负载
-};
